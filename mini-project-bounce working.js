78f069dc2685f26cba44eb620044c7e50e801a2b
AKA-turtle <54418597+AKA-turtle@users.noreply.github.com>
2019-09-25 19:24:47 -0600

    more comments on what's needed

--- mini-project-bounce working.js
+++ mini-project-bounce working.js
@@ -2,12 +2,12 @@
 //animation slows down near apex
 
 input.onButtonPressed(Button.A, function () {
-    for (let i = 0; i < 2; i++) {
-        sprite.turn(Direction.Right, 180)  //cause sprite 
+    for (let i = 0; i < 2; i++) {       //need to flip states. one for moving up other for down
+        sprite.turn(Direction.Right, 180)  //cause sprite to bounce up
         for (let i = 0; i <= 4 - 1; i++) {
             sprite.move(1)
             basic.pause((i + 1) * (i + 1) * 12) //math creates parabala like timing in upward direction only
-                                                //need to create 2 diferent modes
+                                                //need to create 2 diferent states and switch between them
         }
     }
 })
